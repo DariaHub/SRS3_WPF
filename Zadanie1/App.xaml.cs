9d0b18ed81d0ad2221899e024f1ade201ac3a764
DariaHub <pomelova.dasha@mail.ru>
2022-02-11 14:42:47 +0300

    Добавьте файлы проекта.

---
+++ Zadanie1/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Zadanie1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public static string str;
+
+        protected override void OnActivated(EventArgs e)
+        {
+            str += "Activated \n";
+            base.OnActivated(e);
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            str += "Deactivated \n";
+            base.OnDeactivated(e);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            str += "Startup \n";
+            base.OnStartup(e);
+        }
+
+    }
+}
---
+++ Zadanie2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Mutex mutex = new Mutex(false, "MyApplication");
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!mutex.WaitOne(500, false))
+            {
+                MessageBox.Show("Это приложение уже запущено", "Ошибка");
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb_a.Text != "" && tb_b.Text != "")
+            {
+                tb_answer.Text = Convert.ToString(Convert.ToDouble(tb_a.Text) + Convert.ToDouble(tb_b.Text));
+            }
+            else
+                tb_answer.Text = "ошибка";
+        }
+
+        private void btn_minus_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb_a.Text != "" && tb_b.Text != "")
+            {
+                tb_answer.Text = Convert.ToString(Convert.ToDouble(tb_a.Text) - Convert.ToDouble(tb_b.Text));
+            }
+            else
+                tb_answer.Text = "ошибка";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb_a.Text != "" && tb_b.Text != "")
+            {
+                tb_answer.Text = Convert.ToString(Convert.ToDouble(tb_a.Text) * Convert.ToDouble(tb_b.Text));
+            }
+            else
+                tb_answer.Text = "ошибка";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (tb_a.Text != "" && tb_b.Text != ""&& Convert.ToDouble(tb_b.Text)!=0)
+            {
+                tb_answer.Text = Convert.ToString(Convert.ToDouble(tb_a.Text) / Convert.ToDouble(tb_b.Text));
+            }
+            else
+                tb_answer.Text = "ошибка";
+        }
+    }
+}
